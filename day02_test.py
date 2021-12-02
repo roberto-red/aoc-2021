9fd113882ef98596832c06f90292a90c96d7d2d0
Roberto Redradix <roberto@redradix.com>
2021-12-02 15:22:14 +0100

    AoC 2: bootstrap

---
+++ day02_test.py
@@ -0,0 +1,11 @@
+# https://adventofcode.com/2021/day/2
+
+with open("day02.input.txt", "r") as f:
+    input = f.read().splitlines()
+
+example_input = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2""".splitlines()
