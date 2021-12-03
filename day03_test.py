ac386b818d66cd629b766a7117d81f45dc51ed1a
Roberto Redradix <roberto@redradix.com>
2021-12-03 22:15:10 +0100

    AoC 3: bootstrap

---
+++ day03_test.py
@@ -0,0 +1,17 @@
+# https://adventofcode.com/2021/day/3
+
+with open("day03.input.txt", "r") as f:
+    input = f.read().splitlines()
+
+example_input = """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010""".splitlines()
