72108553583cae3bcc0b64df270d99a53faff7aa
Roberto Redradix <roberto@redradix.com>
2021-12-05 15:54:45 +0100

    Green: AoC 5: solve part 1

--- day05_test.py
+++ day05_test.py
@@ -138,4 +138,4 @@ def test_determine_number_of_overlapping_points():
     assert determine_number_of_overlapping_points(example_input) == 5
 
     # Solve AoC 5 part 1
-    assert determine_number_of_overlapping_points(input) == None
+    assert determine_number_of_overlapping_points(input) == 5698
