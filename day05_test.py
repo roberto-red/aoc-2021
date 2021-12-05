8b5ddc4b1a82d014e5092f657946be344b989f9f
Roberto Redradix <roberto@redradix.com>
2021-12-05 14:10:07 +0100

    Red: AoC 5: test_exclude_diagonal_lines

--- day05_test.py
+++ day05_test.py
@@ -29,6 +29,21 @@ def test_parse_line():
     assert parse_line(example_input[3]) == ((2, 2), (2, 1))
 
 
+def exclude_diagonal_lines(lines_of_vent):
+    return lines_of_vent
+
+
+def test_exclude_diagonal_lines():
+    assert exclude_diagonal_lines(map(parse_line, example_input)) == [
+        ((0, 9), (5, 9)),
+        ((9, 4), (3, 4)),
+        ((2, 2), (2, 1)),
+        ((7, 0), (7, 4)),
+        ((0, 9), (2, 9)),
+        ((3, 4), (1, 4)),
+    ]
+
+
 def determine_number_of_overlapping_points(lines_of_vents):
     return 0
 
