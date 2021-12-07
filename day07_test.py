84bf41f115980539eb073bca6a44d2cbc0d1dd55
Roberto Redradix <roberto@redradix.com>
2021-12-07 12:46:20 +0100

    Red: AoC 7: solve part 2

--- day07_test.py
+++ day07_test.py
@@ -38,3 +38,30 @@ def test_calculate_minimal_fuel():
 
     # Solve AoC 7 part 1
     assert calculate_minimal_fuel(parse_input(input)) == 343605
+
+
+def calculate_fuel_fixed(crab_horizontal_positions, final_position):
+    total_fuel = 0
+    for horizontal_position in crab_horizontal_positions:
+        distance = abs(horizontal_position - final_position)
+        total_fuel += distance * (distance + 1) // 2
+
+    return total_fuel
+
+
+def calculate_minimal_fuel_fixed(crab_horizontal_positions):
+    max_position = max(crab_horizontal_positions)
+
+    minimal_fuel = float("inf")
+    for position in range(max_position):
+        fuel = calculate_fuel_fixed(crab_horizontal_positions, position)
+        minimal_fuel = min(minimal_fuel, fuel)
+
+    return minimal_fuel
+
+
+def test_calculate_minimal_fuel_fixed():
+    assert calculate_minimal_fuel_fixed(parse_input(example_input)) == 168
+
+    # Solve AoC 7 part 2
+    assert calculate_minimal_fuel_fixed(parse_input(input)) == None
