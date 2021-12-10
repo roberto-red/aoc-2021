8a8d35df1aa65c28373f63de3d721c237471f71c
Roberto Redradix <roberto@redradix.com>
2021-12-10 11:35:30 +0100

    Red: AoC 10: solve part 2

--- day10_test.py
+++ day10_test.py
@@ -26,7 +26,7 @@ BRACKET_PAIRS = {
 OPENING_BRACKETS = BRACKET_PAIRS.keys()
 CLOSING_BRACKETS = BRACKET_PAIRS.values()
 
-SCORING_TABLE = {
+ERROR_SCORING_TABLE = {
     ")": 3,
     "]": 57,
     "}": 1197,
@@ -66,7 +66,9 @@ def calculate_syntax_error_score(lines):
         if illegal_character != None
     ]
 
-    score = sum(map(lambda character: SCORING_TABLE[character], illegal_characters))
+    score = sum(
+        map(lambda character: ERROR_SCORING_TABLE[character], illegal_characters)
+    )
 
     return score
 
@@ -76,3 +78,73 @@ def test_calculate_syntax_error_score():
 
     # Solve AoC 10 part 1
     assert calculate_syntax_error_score(input) == 442131
+
+
+AUTOCOMPLETE_SCORING_TABLE = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+def autocomplete_brackets(line):
+    openings = []
+    for bracket in line:
+        if bracket in OPENING_BRACKETS:
+            openings.append(bracket)
+        elif bracket == BRACKET_PAIRS[openings[-1]]:
+            openings.pop()
+        else:
+            raise Error("Illegal character found")
+
+    return "".join(map(lambda character: BRACKET_PAIRS[character], reversed(openings)))
+
+
+def test_autocomplete_brackets():
+    assert autocomplete_brackets("[({(<(())[]>[[{[]{<()<>>") == "}}]])})]"
+    assert autocomplete_brackets("[(()[<>])]({[<{<<[]>>(") == ")}>]})"
+    assert autocomplete_brackets("(((({<>}<{<{<>}{[]{[]{}") == "}}>}>))))"
+    assert autocomplete_brackets("{<[[]]>}<{[{[{[]{()[[[]") == "]]}}]}]}>"
+    assert autocomplete_brackets("<{([{{}}[<[[[<>{}]]]>[]]") == "])}>"
+
+
+def calculate_completion_string_score(completion_string):
+    total_score = 0
+    for character in completion_string:
+        character_score = AUTOCOMPLETE_SCORING_TABLE[character]
+        total_score *= 5
+        total_score += character_score
+
+    return total_score
+
+
+def test_calculate_completion_string_score():
+    assert calculate_completion_string_score("}}]])})]") == 288957
+    assert calculate_completion_string_score(")}>]})") == 5566
+    assert calculate_completion_string_score("}}>}>))))") == 1480781
+    assert calculate_completion_string_score("]]}}]}]}>") == 995444
+    assert calculate_completion_string_score("])}>") == 294
+
+
+def calculate_autocomplete_score(lines):
+    completion_strings = [
+        autocomplete_brackets(line)
+        for line in lines
+        if check_first_illegal_character(line) == None
+    ]
+
+    scores = list(map(calculate_completion_string_score, completion_strings))
+    scores.sort()
+
+    # NOTE it's assured there'll be an odd number of incomplete lines
+    middle_score = scores[(len(scores) // 2)]
+
+    return middle_score
+
+
+def test_calculate_autocomplete_score():
+    assert calculate_autocomplete_score(example_input) == 288957
+
+    # Solve AoC 10 part 2
+    assert calculate_autocomplete_score(input) == None
