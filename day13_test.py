8b9f7abbd9657f15e100ae2f2e8991d0d7208689
Roberto Redradix <roberto@redradix.com>
2021-12-13 12:22:47 +0100

    AoC 13: bootstrap

---
+++ day13_test.py
@@ -0,0 +1,26 @@
+# https://adventofcode.com/2021/day/13
+
+with open("day13.input.txt", "r") as f:
+    input = f.read().splitlines()
+
+example_input = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5""".splitlines()
